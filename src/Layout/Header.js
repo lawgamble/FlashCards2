3a0e35bb27c9754745f8553ddd0f5078e08b1b76
lawgamble <lsgrecording@gmail.com>
2021-07-27 11:09:57 -0500

    changed header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className="jumbotron bg-dark">
       <div className="container text-white">
-        <h1 className="display-4">Flashcard-o-matic</h1>
-        <p className="lead">Discover The Flashcard Difference.</p>
+        <h1 className="display-4">Study Flashcards!</h1>
+        <p className="lead">Create, edit and study away!</p>
       </div>
     </header>
   );
